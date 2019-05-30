b5d4e443e518a529ee14ea537c54910ef987f654
kyun1016 <psg9710@naver.com>
2019-05-30 15:54:27 +0900

    change main.js

--- main.js
+++ main.js
@@ -7,8 +7,8 @@ var app = http.createServer(function(request,response){
     }
     if(request.url == '/favicon.ico'){
       response.writeHead(404);
-response.end();
-return;
+      response.end();
+      return;
     }
     response.writeHead(200);
     response.end(fs.readFileSync(__dirname + url));
