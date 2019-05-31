a647bf0ce7278998a2f39d5fa258d308b6329579
kyun1016 <psg9710@naver.com>
2019-05-31 12:01:33 +0900

    title부분을 정적 형태에서 동적으로 변경시켜주었다.
    이제 남은 것은 아래 본문내용을 동적으로 변화시켜주는 것인데,
    이는 본문 내용만 따로 저장해놓은 파일에서 불러오는 형식을 띄게 될 것 같다.

--- web1_html_internet-master/main.js
+++ web1_html_internet-master/main.js
@@ -1,14 +1,20 @@
 var http = require('http');
 var fs = require('fs');
+//동적인 활용을 위한 선언
+//참고, nodejs url parse query string
 var url = require('url');
 
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
+    //동적인 접근을 하도록 설정해준다.
     var queryData = url.parse(request.url, true).query;
+    //가독성을 높이기 위해서 title로 묶어주었다.
+    var title = queryData.id;
     console.log(queryData.id);
+    //기본 홈페이지로 가도록 설정해준다.
     if(_url == '/'){
-      _url = '/index.html';
+      title = 'Welcome';
     }
     if(_url == '/favicon.ico'){
       response.writeHead(404);
@@ -16,7 +22,33 @@ var app = http.createServer(function(request,response){
       return;
     }
     response.writeHead(200);
-    response.end(queryData.id);
+    //템플릿을 활용해 문서를 처리하였다.
+    var template = `
+    <!doctype html>
+    <html>
+    <head>
+      <title>WEB1 - ${title}</title>
+      <meta charset="utf-8">
+    </head>
+    <body>
+      <h1><a href="/">WEB</a></h1>
+      <ol>
+        <li><a href="/?id=HTML">HTML</a></li>
+        <li><a href="/?id=CSS">CSS</a></li>
+        <li><a href="/?id=JavaScript">JavaScript</a></li>
+      </ol>
+      <h2>${title}</h2>
+      <p><a href="https://www.w3.org/TR/html5/" target="_blank" title="html5 speicification">Hypertext Markup Language (HTML)</a> is the standard markup language for <strong>creating <u>web</u> pages</strong> and web applications.Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
+      <img src="coding.jpg" width="100%">
+      </p><p style="margin-top:45px;">HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects, such as interactive forms, may be embedded into the rendered page. It provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes and other items. HTML elements are delineated by tags, written using angle brackets.
+      </p>
+    </body>
+    </html>
+    `;
+
+
+
+    response.end(template);
 
 });
 app.listen(3000);
