652b3f4423189575a3656394e81a3efd78a96cfd
kyun1016 <psg9710@naver.com>
2019-05-31 12:30:17 +0900

    본문 동적 선언 완료

--- nodejsFileRead/fileread.js
+++ nodejsFileRead/fileread.js
@@ -3,6 +3,8 @@
 //https://nodejs.org/dist/latest-v6.x/docs/api/fs.html#fs_fs_readfile_file_options_callback
 var fs = require('fs');
 
+//여기 보면 nodejsFileRead/sample로 해서 들어갔는데, 이는 처음 시작주소가 nodejs여서 이다.
+//만일 시작 노드가 nodejsFileRead 였다면, 그냥 sample.txt로 해도 상관 없다.
 fs.readFile('nodejsFileRead/sample.txt', 'utf-8',(err, data) =>{
   if (err) throw err;
   console.log(data);
